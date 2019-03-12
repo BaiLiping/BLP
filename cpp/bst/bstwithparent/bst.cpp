4b7a806f0b63a56a2cef9d56bd2c43fa659a9453
BaiLiping <blp_engineer@outlook.com>
2019-03-12 13:35:06 +0800

    matrix print of a binary tree

---
+++ cpp/bst/bstwithparent/bst.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<vector>
+#include<algorithm>
+#include"bst.hpp"
+using namespace std;
+template<typename T>
+BST<T>::BST():root(NULL){
+    cout<<"a bst has been constructed"<<endl;
+}
+template<typename T>
+BST<T>::~BST(){
+    cout<<"bst has been destroyed"<<endl;
+}
+template<typename T>
+void BST<T>::in_order_traverse(Node<T> * treenode){
+    if(treenode){
+        in_order_traverse(treenode->leftChild);
+        cout<<treenode->data<<endl;
+        in_order_traverse(treenode->rightChild);
+    }
+}
+template<typename T>
+void BST<T>::print(){
+    in_order_traverse(root);
+}
+template<typename T>
+int BST<T>::get_level(Node<T>* treenode){
+    if(treenode==NULL) return 0;
+    if(treenode->leftChild==NULL&&treenode->rightChild==NULL) {
+        treenode->level=1;
+        return 1;
+    }
+    else{
+        int left_level=0;
+        int right_level=0;
+        int result=0;
+        if(treenode->leftChild!=NULL) left_level=treenode->leftChild->level;
+        if(treenode->rightChild!=NULL) right_level=treenode->rightChild->level;
+        if(left_level==0&&right_level==0){
+            left_level=get_level(treenode->leftChild);
+            right_level=get_level(treenode->rightChild);
+        }
+        result=1+max(left_level,right_level);
+        treenode->level=result;
+        return result;
+
+    }
+        
+}
+template<typename T>
+void BST<T>::set_level(){
+    get_level(root);
+}
+template<typename T>
+void BST<T>::insert(T val){
+    Node<T> * newnode=new Node<T>(val);
+    Node<T> * leading=root;
+    Node<T> * trailing=leading;
+    while(leading){
+        trailing=leading;
+        if(val<leading->data) leading=leading->leftChild;
+        else leading=leading->rightChild;
+    }
+    if(trailing==NULL) root=newnode;
+    else{
+        newnode->parent=trailing;
+        if(val<trailing->data) trailing->leftChild=newnode;
+        else trailing->rightChild=newnode;
+    }
+}
+template<typename T>
+void BST<T>::build_tree(vector<T> & input_data){
+    for(auto x:input_data){
+        insert(x);
+    }
+    set_level();    
+}
+template<typename T>
+void BST<T>::matrix_print(){
+int height=root->level;
+int n=pow(2,height)-1;
+vector<vector<string>> result(height,vector<string>(n,"  "));
+
+layout(root,result,0,(n+1)/2,(n+1)/2);
+int i=0;
+for (auto x:result){
+    for (auto y:result[i]){
+        cout<<y;
+    }
+    cout<<endl;
+    ++i;
+}
+
+}
+template<typename T>
+void BST<T>::layout(Node<T>* treenode, vector<vector<string>> &result,int layer, int pos, int base){
+    if(treenode==NULL) return;
+    result[layer][pos]=to_string(treenode->data);
+    layout(treenode->leftChild,result,layer+1,pos-base/2,base/2);
+    layout(treenode->rightChild,result,layer+1,pos+base/2,base/2);
+}
---
+++ cpp/bst/bstwithparent/test.cpp
@@ -0,0 +1,14 @@
+
+#include<iostream>
+#include<vector>
+#include "bst.hpp"
+using namespace std;
+
+int main(){
+    vector<int> input={8,12,4,9,2,11,1,3,5,10,7,15,6};
+    BST<int> bst;
+    bst.build_tree(input);
+    bst.print();
+    bst.matrix_print();
+    return 0;
+}
