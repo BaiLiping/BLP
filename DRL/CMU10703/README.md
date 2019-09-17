220c7cd0ca6fbc0c3200feff688be838a43862ce
BaiLiping <blp_engineer@outlook.com>
2019-09-17 06:09:26 -0700

    sep 17

---
+++ DRL/CMU10703/README.md
@@ -0,0 +1,2 @@
+# CMU10703_Assignments
+Assignments for CMU 10703: Deep Reinforcement Learning and Control
--- DRL/reinforcement-learning/blp/pa/env.py
+++ DRL/reinforcement-learning/blp/pa/env.py
@@ -4,53 +4,45 @@ import sys
 wave_length=0.7
 phase=180/5
 
-
-
 def generate_target():
-    index=np.random.choice(np.arange(48))
+    index=np.random.choice(range(48))
     position=np.unravel_index(index,(8,6))
     return position
     
 def location_info(target_location):
     distance=math.sqrt(pow((3*(target_location[0])),2)+pow(3*(target_location[1]),2))
-    impact=math.cos(distance/wave_length)
+    impact=(1/(distance+1))*30*math.cos(distance/wave_length)
     return impact
         
-def compute_impact(state,target_location):
-    impact=0
-    for i in range(len(state)):
-        if i!=0:
-            x=np.unravel_index(i,(8,6))[0]
-            y=np.unravel_index(i,(8,6))[1]
-            distance=math.sqrt(pow((3*(x-target_location[0])),2)+pow(3*(y-target_location[1]),2))
-            impact+=math.cos(distance/wave_length+(state[i])*phase)
-    return impact
+def compute_reward(x,y,p,target_location):
+    distance=math.sqrt(pow((3*(x-target_location[0])),2)+pow(3*(y-target_location[1]),2))
+    reward=(1/(distance+1))*30*math.cos(distance/wave_length+p*phase)
+    return reward
 
 class PhasedArrayEnv(object):
     def __init__(self):
-        wave_length=0.7
-        phase=180/5
-        nA=47*5
-        nS=48*47*6
-        state=np.zeros(48)
-        target_location=generate_target()
+        self.wave_length=0.7
+        self.phase=180/5
+        self.nA=47*5
+        #self.nS=48*47*6
+        self.state=np.zeros(48)
+        self.target_location=generate_target()
     
     def reset(self):
         self.target_location=generate_target()
         self.state=np.zeros(48)
         self.state[0]=location_info(self.target_location)
-       #print(self.state[0])
-       
+    
+    def get_nA(self):
+        return self.nA
+
     def step(self,action):
-        action_index=np.argmax(action)
-        impact=compute_impact(self.state,self.target_location)
-        x=np.unravel_index(action_index,(8,6,5))[0]
-        y=np.unravel_index(action_index,(8,6,5))[1]
-        p=np.unravel_index(action_index,(8,6,5))[2]+1
+        x=np.unravel_index(action,(8,6,5))[0]
+        y=np.unravel_index(action,(8,6,5))[1]
+        p=np.unravel_index(action,(8,6,5))[2]+1
         state_index=np.ravel_multi_index((y,x),(8,6))
         self.state[state_index]=p
-        new_impact=compute_impact(self.state,self.target_location)
-        reward=new_impact-impact
+        reward=compute_reward(x,y,p,self.target_location)
         return self.state,reward
     
     def render(self):
