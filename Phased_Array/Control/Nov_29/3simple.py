cd2ecbf07fece72f27a21e5b0712f9093ca2eb97
BaiLiping <blp_engineer@outlook.com>
2019-12-01 09:16:28 +0800

    Dec

---
+++ Phased_Array/Control/Nov_29/3simple.py
@@ -0,0 +1,118 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import gym
+import sys
+from envsimple import PhasedArrayEnv
+import torch
+from torch import nn
+from torch import optim
+
+env = PhasedArrayEnv()
+class policy_estimator():
+    def __init__(self, env):
+        self.n_inputs = env.get_nS()
+        self.n_outputs = env.get_nA()
+
+        # Define network
+        self.network = nn.Sequential(
+            nn.Linear(self.n_inputs, 16),
+            nn.ReLU(),
+            nn.Linear(16, self.n_outputs),
+            nn.Softmax(dim=-1))
+
+    def predict(self, state):
+        action_probs = self.network(torch.FloatTensor(state))
+        print(action_probs)
+        action_probs_clip=torch.clamp(action_probs,1e-12, 1-1e-12)
+
+        return action_probs_clip
+pe=policy_estimator(env)
+def discount_rewards(rewards, gamma=1):
+    r = np.array([gamma**i * rewards[i]
+                  for i in range(len(rewards))])
+    # Reverse the array direction for cumsum and then
+    # revert back to the original order
+    r = r[::-1].cumsum()[::-1]
+    return r - r.mean()
+
+def reinforce(env, policy_estimator, num_episodes=20000,
+              batch_size=20, gamma=1):
+
+    # Set up lists to hold results
+    total_rewards = []
+    batch_rewards = []
+    batch_actions = []
+    batch_states = []
+    batch_counter = 1
+    
+    # Define optimizer
+    optimizer = optim.Adam(policy_estimator.network.parameters(), 
+                           lr=0.01)
+    
+    action_space = np.arange(env.get_nA())
+    for ep in range(num_episodes):
+        s_0 = env.reset()
+        states = []
+        rewards = []
+        actions = []
+        for i in range(8):
+            # Get actions and convert to numpy array
+            action_probs = policy_estimator.predict(s_0).detach().numpy()
+            action = np.random.choice(action_space, p=action_probs)
+            s_1, r = env.step(action)
+            
+            states.append(s_0)
+            rewards.append(r)
+            actions.append(action)
+            s_0 = s_1
+            
+        # If complete, batch data
+        batch_rewards.extend(discount_rewards(rewards, gamma))
+        batch_states.extend(states)
+        batch_actions.extend(actions)
+        batch_counter += 1
+        total_rewards.append(sum(rewards))
+        
+        # If batch is complete, update network
+        if batch_counter == batch_size:
+            optimizer.zero_grad()
+            state_tensor = torch.FloatTensor(batch_states)
+            reward_tensor = torch.FloatTensor(batch_rewards)
+            # Actions are used as indices, must be LongTensor
+            action_tensor = torch.LongTensor(batch_actions)
+            
+            # Calculate loss
+            logprob = torch.log(
+                policy_estimator.predict(state_tensor))
+            selected_logprobs = reward_tensor * \
+                logprob[np.arange(len(action_tensor)), action_tensor]
+            loss = -selected_logprobs.mean()
+            
+            # Calculate gradients
+            loss.backward()
+            # Apply gradients
+            optimizer.step()
+            
+            batch_rewards = []
+            batch_actions = []
+            batch_states = []
+            batch_counter = 1
+                
+            # Print running average
+        print("\rEp: {} Average of last 10: {:.2f}".format(
+            ep + 1, np.mean(total_rewards[-10:])))
+                
+    return total_rewards
+
+rewards = reinforce(env, pe)
+window = 10
+smoothed_rewards = [np.mean(rewards[i-window:i+1]) if i > window
+                    else np.mean(rewards[:i+1]) for i in range(len(rewards))]
+
+plt.figure()
+plt.plot(rewards)
+plt.plot(smoothed_rewards)
+plt.ylabel('Total Rewards')
+plt.xlabel('Episodes')
+plt.show()
+
---
+++ Phased_Array/Control/Nov_29/A2C.py
@@ -0,0 +1,135 @@
+
+import math
+import random
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.distributions import Categorical
+import matplotlib.pyplot as plt
+
+from env import PhasedArrayEnv
+
+env = PhasedArrayEnv()
+
+plt.ion()
+
+class ActorCritic(nn.Module):
+    def __init__(self, num_inputs, num_outputs, hidden_size, std=0.0):
+        super(ActorCritic, self).__init__()
+        
+        self.critic = nn.Sequential(
+            nn.Linear(num_inputs, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, 1)
+        )
+        
+        self.actor = nn.Sequential(
+            nn.Linear(num_inputs, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, num_outputs),
+            nn.Softmax(dim=1),
+        )
+        
+    def forward(self, x):
+        value = self.critic(x)
+        probs = self.actor(x)
+        dist  = Categorical(probs)
+        return dist, value
+
+
+def test_env(vis=False):
+    state = env.reset()
+    total_reward = 0
+    for i in range(10):
+        state = torch.FloatTensor(state)
+        dist, _ = model(state)
+        next_state, reward = env.step(dist.sample().numpy()[0])
+        state = next_state
+        total_reward += reward
+    return total_reward
+
+
+def compute_returns(next_value, rewards, masks, gamma=0.99):
+    R = next_value
+    returns = []
+    for step in reversed(range(len(rewards))):
+        R = rewards[step] + gamma * R * masks[step]
+        returns.insert(0, R)
+    return returns
+
+def plot(frame_idx, rewards):
+    plt.plot(rewards,'b-')
+    plt.title('frame %s. reward: %s' % (frame_idx, rewards[-1]))
+    plt.pause(0.0001)
+
+
+num_inputs  = env.get_nS()
+num_outputs = env.get_nA()
+
+#Hyper params:
+hidden_size = 256
+lr          = 1e-3
+num_steps   = 5
+
+model = ActorCritic(num_inputs, num_outputs, hidden_size)
+optimizer = optim.Adam(model.parameters())
+
+
+max_frames   = 20000
+frame_idx    = 0
+test_rewards = []
+
+
+state = env.reset()
+
+while frame_idx < max_frames:
+
+    log_probs = []
+    values    = []
+    rewards   = []
+    masks     = []
+    entropy = 0
+
+    # rollout trajectory
+    for _ in range(num_steps):
+        state = torch.FloatTensor(state)
+        dist, value = model(state)
+
+        action = dist.sample()
+        next_state, reward = env.step(action.numpy())
+
+        log_prob = dist.log_prob(action)
+        entropy += dist.entropy().mean()
+        
+        log_probs.append(log_prob)
+        values.append(value)
+        rewards.append(torch.FloatTensor(reward))
+        masks.append(torch.FloatTensor(1 - done))
+        
+        state = next_state
+        frame_idx += 1
+        
+        if frame_idx % 100 == 0:
+            test_rewards.append(np.mean([test_env() for _ in range(10)]))
+            plot(frame_idx, test_rewards)
+            
+    next_state = torch.FloatTensor(next_state)
+    _, next_value = model(next_state)
+    returns = compute_returns(next_value, rewards, masks)
+    
+    log_probs = torch.cat(log_probs)
+    returns   = torch.cat(returns).detach()
+    values    = torch.cat(values)
+
+    advantage = returns - values
+
+    actor_loss  = -(log_probs * advantage.detach()).mean()
+    critic_loss = advantage.pow(2).mean()
+
+    loss = actor_loss + 0.5 * critic_loss - 0.001 * entropy
+
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
